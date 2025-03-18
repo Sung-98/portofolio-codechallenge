88c9fdacf3196c7bcd34b50d7b41c196bd939249
Hendy Sung <hendysung@Hendy.local>
2025-03-18 15:56:16 +0700

    change1

--- src/app/components/contact.tsx
+++ src/app/components/contact.tsx
@@ -1,34 +1,70 @@
 export default function Contact() {
-    return (
-        <section id="contact" className="pt-36 pb-32">
-            <div className="container">
-                <div className="w-full px-4">
-                    <div className="max-w-xl mx-auto text-center mb-16">
-                        <h4 className="font-semibold text-lg text-primary font-serif italic mb-2">Contact</h4>
-                        <h2 className="font-bold text-dark text-3xl mb-4 sm:text-4xl lg:text-5xl">Hubungi Kami</h2>
-                        <p className="font-medium text-md text-slate-500 md:text-lg">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam id sodales ipsum.</p>
-                    </div>
-                </div>
-                <form>
-                   <div className="w-full lg:w-2/3 lg:mx-auto"> 
-                    <div className="w-full px-4 mb-8">
-                        <label htmlFor="name" className="text-base font-bold text-primary">Nama</label>
-                        <input type="text" id="name" className="w-full bg-slate-200 text-dark p-3 rounded-md focus:outline-none focus:ring-primary focus:ring-1 focus:border-primary"/>
-                    </div>
-                    <div className="w-full px-4 mb-8">
-                        <label htmlFor="email" className="text-base font-bold text-primary">Email</label>
-                        <input type="email" id="email" className="w-full bg-slate-200 text-dark p-3 rounded-md focus:outline-none focus:ring-primary focus:ring-1 focus:border-primary"/>
-                    </div>
-                    <div className="w-full px-4 mb-8">
-                        <label htmlFor="message" className="text-base font-bold text-primary">Pesan</label>
-                        <textarea typeof="email" id="email" className="w-full bg-slate-200 text-dark p-3 rounded-md focus:outline-none focus:ring-primary focus:ring-1 focus:border-primary h-32"></textarea>
-                    </div>
-                    <div className="w-full">
-                        <button className="text-base font-semibold text-white bg-primary py-3 px-8 rounded-full w-full hover:opacity-80 hover:shadow-lg transisiton duration-500">Kirim</button>
-                    </div>
-                   </div> 
-                </form>
+  return (
+    <section id="contact" className="pt-36 pb-32">
+      <div className="container">
+        <div className="w-full px-4">
+          <div className="max-w-xl mx-auto text-center mb-16">
+            <h4 className="font-semibold text-lg text-primary font-serif italic mb-2">
+              Contact
+            </h4>
+            <h2 className="font-bold text-dark text-3xl mb-4 sm:text-4xl lg:text-5xl">
+              Hubungi Kami
+            </h2>
+            <p className="font-medium text-md text-slate-500 md:text-lg">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam id
+              sodales ipsum.
+            </p>
+          </div>
+        </div>
+        <form>
+          <div className="w-full lg:w-2/3 lg:mx-auto">
+            <div className="w-full px-4 mb-8">
+              <label
+                htmlFor="name"
+                className="text-base font-bold text-primary"
+              >
+                Nama
+              </label>
+              <input
+                type="text"
+                id="name"
+                className="w-full bg-slate-200 text-dark p-3 rounded-md focus:outline-none focus:ring-primary focus:ring-1 focus:border-primary"
+              />
             </div>
-        </section>
-    )
+            <div className="w-full px-4 mb-8">
+              <label
+                htmlFor="email"
+                className="text-base font-bold text-primary"
+              >
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                className="w-full bg-slate-200 text-dark p-3 rounded-md focus:outline-none focus:ring-primary focus:ring-1 focus:border-primary"
+              />
+            </div>
+            <div className="w-full px-4 mb-8">
+              <label
+                htmlFor="message"
+                className="text-base font-bold text-primary"
+              >
+                Pesan
+              </label>
+              <textarea
+                typeof="email"
+                id="email"
+                className="w-full bg-slate-200 text-dark p-3 rounded-md focus:outline-none focus:ring-primary focus:ring-1 focus:border-primary h-32"
+              ></textarea>
+            </div>
+            <div className="w-full">
+              <button className="text-base font-semibold text-white bg-primary py-3 px-8 rounded-full w-full hover:opacity-50 hover:shadow-lg transisiton duration-500">
+                Kirim
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </section>
+  );
 }
